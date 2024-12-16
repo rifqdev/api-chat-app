6fcf873ddacdcaf188c97bbc319fc3c725064dd9
rifqiay <mrifqiay@gmail.com>
2024-12-16 10:37:05 +0700

    initialize socket io

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
+require("dotenv").config();
 const express = require("express");
 const package = require("./package.json");
 const app = express();
 const cors = require("cors");
-const PORT = process.env.PORT || 3000;
-
+const http = require("http");
+const { Server } = require("socket.io");
 const sequelize = require("./src/config/sequelize");
 const routes = require("./src/routes/index.routes");
 
+const PORT = process.env.PORT || 3000;
+
+// Buat HTTP Server
+const server = http.createServer(app);
+
+// Inisialisasi Socket.IO
+const io = new Server(server, {
+  cors: {
+    origin: "*", // URL Frontend
+    methods: ["GET", "POST"],
+  },
+});
+
+// Test koneksi ke database
 sequelize.testConnection();
 
+// Middleware
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-app.use(cors({ origin: process.env.FRONTEND_URL }));
-
+// Endpoint Default
 app.use("/", (req, res) => {
   res.json({
     app: package.name,
@@ -23,6 +39,22 @@ app.use("/", (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+// Konfigurasi Socket.IO
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("send_message", (data) => {
+    console.log("Message received:", data);
+
+    io.emit(data.to, data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+// Jalankan Server
+server.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
 });
