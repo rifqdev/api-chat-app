7edcba19452b122514cdde137016bd4b01b7a979
rifqiay <mrifqiay@gmail.com>
2024-12-18 11:17:55 +0700

    create api change photo profile

--- src/modules/users/user.controllers.js
+++ src/modules/users/user.controllers.js
@@ -1,11 +1,13 @@
 const Users = require("./users.model");
 const wrapper = require("../../utils/wrapper");
+const { uploadToMinio, deleteFile } = require("../../utils/upload");
 
 const getUser = async (req, res) => {
   try {
     const userId = req.user.id;
     const user = await Users.findOne({ where: { id: userId } });
     delete user.dataValues.password;
+    user.dataValues.photo = user.dataValues.photo ? `http://${process.env.MINIO_ENDPOINT}/${user.dataValues.photo}` : null;
     return wrapper.successResponse(res, user, "User fetched successfully", 200);
   } catch (error) {
     return wrapper.errorResponse(res, error.message, 400);
@@ -57,4 +59,31 @@ const updateUsername = async (req, res) => {
   }
 };
 
-module.exports = { getUser, updateBio, updateFullname, updateUsername };
+const updateProfilePicture = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const getUser = await Users.findOne({ where: { id: userId } });
+
+    const file = req.file;
+    if (!file) {
+      return wrapper.errorResponse(res, "Photo is required", 400);
+    }
+    const result = await uploadToMinio("chat-app-profile-picture", file.path, file.originalname);
+    if (result.error) {
+      return wrapper.errorResponse(res, result.error, 400);
+    }
+    if (getUser.photo) {
+      await deleteFile("chat-app-profile-picture", getUser.dataValues.photo.split("/").pop());
+    }
+    const payload = {
+      photo: result.url,
+    };
+    await Users.update(payload, { where: { id: userId } });
+    return wrapper.successResponse(res, payload, "Profile picture updated successfully", 200);
+  } catch (error) {
+    return wrapper.errorResponse(res, error.message, 400);
+  }
+};
+
+module.exports = { getUser, updateBio, updateFullname, updateUsername, updateProfilePicture };
--- src/modules/users/user.routes.js
+++ src/modules/users/user.routes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("../../middlewares/jwt");
 const userController = require("./user.controllers");
+const { upload } = require("../../utils/upload");
 
 router.get("/details", jwt.verifyToken, userController.getUser);
 router.put("/update-bio", jwt.verifyToken, userController.updateBio);
 router.put("/update-fullname", jwt.verifyToken, userController.updateFullname);
 router.put("/update-username", jwt.verifyToken, userController.updateUsername);
+router.put("/update-profile-picture", jwt.verifyToken, upload.single("photo"), userController.updateProfilePicture);
 
 module.exports = router;
